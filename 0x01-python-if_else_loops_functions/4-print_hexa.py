4dc040a8b60b622efcc78b4db028cacd6d7f2df2
G3rt92 <beukesbg@gmail.com>
2022-11-30 05:53:03 +0200

    fixed: identation

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for hex_number in range(0, 99):
-    print(f"{hex_number} = {hex(hex_number)}")
+    print("{} = {}".format(number, hex(number)))
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-	"""Print the last digit of a number and return"""
-	ldigit = abs(number) % 10
-	print(ldigit, end="")
-	return (ldigit)
+    """Print the last digit of a number and return"""
+    ldigit = abs(number) % 10
+    print(ldigit, end="")
+    return (ldigit)
