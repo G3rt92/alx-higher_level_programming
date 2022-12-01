a656e75a6010a026ac35106ae1819cb07b7bcdcd
G3rt92 <beukesbg@gmail.com>
2022-12-01 14:21:41 +0200

    fixed: output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ import sys
 if __name__ == "__main__":
     n = len(sys.argv) - 1
     if n == 1:
-        print("1 argument.")
+        print("1 argument:")
     elif n == 0:
-        print("0 arguments:")
+        print("0 arguments.")
     else:
         print("{} arguments:".format(n))
     for i in range(n):
