86d8f70dc4a9f9738f7e8a258c86eed7ee59aa12
G3rt92 <beukesbg@gmail.com>
2022-12-01 15:26:20 +0200

    fixed:the import

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     """Match bytecode as the Python bytecode."""
-    from magic_calculator_102 import add, sub
+    from magic_calculation_102 import add, sub
     if a < b:
         sum_ = add(a, b)
         for i in range(4, 6):
