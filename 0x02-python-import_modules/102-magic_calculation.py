922cf74b3d8af091599305945a7e7853bc5c7ee6
G3rt92 <beukesbg@gmail.com>
2022-12-01 15:16:28 +0200

    advanced

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub
+from magic_calculator_102 import add, sub
 def magic_calculation(a, b):
     """Match bytecode as the Python bytecode."""
     if a < b:
