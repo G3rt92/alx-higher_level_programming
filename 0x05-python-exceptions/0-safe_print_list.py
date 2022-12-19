6e7aa9aeb6d8958632e8e81dc1afdefd3d2a6f2d
G3rt92 <beukesbg@gmail.com>
2022-12-19 19:20:13 +0200

    fixed: requirement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+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#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
     for elem in range(x):
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             break
         else:
-            i+=1
+            i += 1
     print()
     return i
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
         except (ValueError, TypeError):
-            print("not an integer")
+            pass
         else:
-            i+=1
+            i += 1
     print()
     return i
